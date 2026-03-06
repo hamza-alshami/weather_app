abf7a09c97b8f26969101afee3c6f51389f69374
Hamza Al-shami <ham.za.moh.sha@gmail.com>
2026-03-06 16:57:54 +0300

    adding bloc and cubit state management but not compleated yet

---
+++ lib/cubits/get_weather_cubitز/get_weather_cubit.dart
@@ -0,0 +1,6 @@
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:weather_app/cubits/get_weather_cubit%D8%B2/get_weather_state.dart';
+
+class GetWeatherCubit extends Cubit<WeatherState> {
+  GetWeatherCubit(super.initialState);
+}
